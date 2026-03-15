228d31ce245a6025b91c34942fcfc587ae7179b9
rm364347 <rm364347@fiap.com.br>
2026-03-15 03:48:32 -0300

    Update App.js
    37CLD

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
 
 
-          <h1 className="App-title">Essa é Feature 11CLC F2 2026</h1>
+          <h1 className="App-title">Essa é Feature 37CLD F2 2026</h1>
 
-          <h1 className="App-title">Essa é Feature 11CLC F1 2026</h1>
+          <h1 className="App-title">Essa é Feature 37CLD F1 2026</h1>
 
 
       </header>
